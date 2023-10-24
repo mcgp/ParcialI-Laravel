e678a8a56904adaad28c581c1b24f937039ab0e6
mcgp <51185772+mcgp@users.noreply.github.com>
2023-10-23 22:23:54 -0500

    Ajuste de visualizacion de los cursos

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -8,6 +8,11 @@
             @foreach ($cursos as $curso)
                 <li class="list-group-item">
                    !- <a >{{ $curso->nombre }}</a>
+                   <p><strong>Descripción:</strong> {{ $curso->descripcion }}</p>
+                   <p><strong>Duración:</strong> {{ $curso->duracion }} horas</p>
+                   <p><strong>Precio:</strong> ${{ number_format($curso->precio, 2) }}</p>
+                     <p><strong>Fecha inicio:</strong> {{ $curso->fecha_inicio }}</p>
+
                    <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-primary">Editar</a>
                    <form method="POST" action="{{ route('cursos.destroy', $curso->id) }}">
                     @csrf
@@ -21,10 +26,10 @@
         <p>No hay cursos disponibles.</p>
     @endif
 
-    <a href="{{ route('cursos.create') }}" class="btn btn-primary">Crear curso</a>
+    <a href="{{ route('cursos.create') }}" class="btn btn-success">Crear curso</a>
     <form action="{{ route('cursos.index') }}" method="GET">
         <input type="text" name="filtro" placeholder="Buscar...">
-        <button type="submit">Buscar</button>
+        <button type="submit">Buscar por nombre o descripcion</button>
     </form>
     
 @endsection
